53939a163459d55cbe0c6b2189b2d3c6c473d90c
Scott_N_Yang <1192426931@qq.com>
2019-01-07 10:37:42 +0800

    IU-2018.3.1 <YangNing@YangNing-PC Create markdown.xml, p3c.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default copy.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, _@user_Darcula.icls, mavenVersion.xml, Default.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, lombok-plugin.xml, user.xml, other.xml, androidStudioFirstRun.xml, code.style.schemes, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, webDeployTransportHostTargets.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, consoleFolding.xml, laf.xml, KeyPromoterXStatistic.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, intentionSettings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+*
+* ${description}
+*
+* @author ${USER}
+*
+* @create ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+**/
+public class ${NAME} {
+}
